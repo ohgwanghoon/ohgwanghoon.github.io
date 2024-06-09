a66ab48ea7619f086cd8520ca824fa8fe2f69f54
ohgwanghoon <gwanghun3634@naver.com>
2024-06-09 18:08:53 +0900

    html renderer

--- yorijori/flutter_bootstrap.js
+++ yorijori/flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"1cdbebee19012cee6cfaf67a434e3a6fcbc4682b","builds":[{"compileTarget":"dart2js","renderer":"auto","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"1cdbebee19012cee6cfaf67a434e3a6fcbc4682b","builds":[{"compileTarget":"dart2js","renderer":"html","mainJsPath":"main.dart.js"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3451759917"
+    serviceWorkerVersion: "3397180419"
   }
 });
